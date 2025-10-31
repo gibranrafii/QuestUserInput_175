94b91d363ac62ce15bee1e0df49583b04ca904d8
gibranrafii <gibranrafi21@gmail.com>
2025-10-31 10:09:59 +0700

    membuat variabel untuk menyimpan data yang diperoleh dari komponen UI

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -35,4 +35,11 @@ fun FormDataDiri(modifier: Modifier){
     var textAlamat by remember { mutableStateOf("") }
     var textJK by remember { mutableStateOf("") }
 
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
+    val gender:List<String> = listOf("Laki-laki","Perempuan")
+
 }
